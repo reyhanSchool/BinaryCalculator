b7a9c4592219d37fc4a30148b447637f284e6dd1
reyhanSchool <94261041+reyhanSchool@users.noreply.github.com>
2023-01-27 21:30:16 -0500

    Delete target directory
    removed target folder

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main.java.com.ontariotechu.sofe3980U","l":"App"},{"p":"main.java.com.ontariotechu.sofe3980U","l":"Binary"}];updateSearchResults();
